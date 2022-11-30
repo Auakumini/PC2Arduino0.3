a7c824daf857e558ff5d94031b92f05fd2c22691
nudeboy69 <98754395+nudeboy69@users.noreply.github.com>
2022-11-30 09:45:13 +0100

    Add files via upload

---
+++ PC2Arduino/main.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <Windows.h>
+#include "SerialPort.h"
+#include "windows.h"
+#include "Morser.h"
+//#include "Morser.cpp"
+
+int main();
+int toArd(string command);
+int attrToArd();
